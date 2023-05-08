1ac102fea373766b96a5b073bf62a7b9d1730a93
KarlTred265 Tred <tred26559@gmail.com>
2023-05-08 17:14:26 +0000

    Add superuser and admin

--- newsmedia/models.py
+++ newsmedia/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.urls import reverse
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    is_public = models.BooleanField(default=True)
+    featured = models.BooleanField(default=False)
+    views = models.PositiveIntegerField(default=0)
+    allow_comments = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.slug])
+
+    def get_like_count(self):
+        return self.likes.count()
+
+    def is_liked_by_user(self, user):
+        return self.likes.filter(id=user.id).exists()
+
+    def increment_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name} ({self.email})'
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.post.slug])
+
