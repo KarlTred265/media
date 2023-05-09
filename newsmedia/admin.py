162c00997c472a108427c8b3a92e57658e6f429f
KarlTred265 Tred <tred26559@gmail.com>
2023-05-09 12:09:35 +0000

    Add this

--- newsmedia/admin.py
+++ newsmedia/admin.py
@@ -5,5 +5,19 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
-    summernote_fields = ('content')
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created_on', 'approved')
+    list_filter = ('post', 'created_on', 'approved')
+    search_fields = ('name', 'email', 'body')
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+    approve_comments.short_description = "Approve selected comments"
